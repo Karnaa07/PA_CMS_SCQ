32377704e82c1082c68b1ba61206a2bdac27197d
clemYvars <clem.yvars@gmail.com>
2022-02-28 21:14:51 +0100

    dashboard grille

--- www/View/dashboard.view.php
+++ www/View/dashboard.view.php
@@ -1,14 +1,23 @@
 <link href="../css/dashboard.css" rel="stylesheet">
 <body class="container">
-    <section class="bar-stat">
+    <!-- <section class="bar-stat">
         <div class="utilisateurs-stat">
+            <h1>Utilisateurs</h1>
+        </div>
+        <div class="utilisateurs-stat">
+            <h1>Clics</h1>
+        </div>
+        <div class="utilisateurs-stat">
+             <h1>Articles postés</h1>
+        </div>
+    </section> -->
+<div class="wrapper">
+  <div class="one">Un</div>
+  <div class="two">Deux</div>
+  <div class="three">Trois</div>
+  <div class="four">Quatre</div>
+  <div class="five">Cinq</div>
+  <div class="six">Six</div>
+</div>
 
-        </div>
-        <div class="utilisateurs-stat">
-            
-        </div>
-        <div class="utilisateurs-stat">
-            
-        </div>
-    </section>
 </body>
