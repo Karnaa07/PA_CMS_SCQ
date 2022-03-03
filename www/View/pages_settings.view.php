3d7df3c1f544f2b57df289d2981af44d88b5dcb2
GIRARD Quentin <quentingrd0706@gmail.com>
2022-03-03 19:27:30 +0100

    front pages

---
+++ www/View/pages_settings.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Pages_Settings</title>
+    <link href="../css/pages_settings.css" rel="stylesheet">
+</head>
+<body>
+    <section class="section1">
+        <div class="container_view_pages">
+            <article class="benefit">
+                <a href="#">    
+                    <img src="../data/img/create_pages.svg">
+                </a>
+                <a href="#">
+                    <img src="../data/img/modifie_pages.svg">
+                </a>
+                <a href="#">
+                    <img src="../data/img/settings_pages_log.svg">
+                </a>
+                <a href ="#">
+                    <img src="../data/img/delete_pages.svg">
+                </a>
+            </article>
+        </div>
+    </section>
+    <section class = "section2">
+    
+    </section>
+</body>
+</html>
