50715393c6bb0b1cf78da68f0c9be908c5567aff
CamilleBorra <cborra@hotmail.fr>
2022-05-18 21:28:03 +0200

    fixMerge

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -10,30 +10,8 @@ use App\Core\Mail;
 
 class User {
 
-    public function login() //login
+    public function login() 
     {
-<<<<<<< HEAD
-
-        $Mail= new Mail();
-        $Mail->send_mail();
-
-        $user = new UserModel();
-        if(!empty($_POST))
-        {
-            $user->setUser();
-            $exist = $user->exist_user($_POST["email"],$_POST["password"]);
-            if($exist["id"]){
-                session_start();
-                $_SESSION['id'] = $exist['id'];
-                $_SESSION['firstname'] = $exist['firstname']; 
-                $view = new View("dashboard","back");
-            }
-            else
-            {
-                echo("Nom de compte ou mot de passe incorrect");
-            }
-
-=======
         if(!isset($_COOKIE['token']))
         {
             $user = new UserModel();
@@ -43,7 +21,8 @@ class User {
                     $user->setUser();              
                     $exist = $user->exist_user($_POST["email"],$_POST["password"]);
                     if($exist["id"]){
-                        header('Location: dashboard');
+                        $_SESSION['firstname'] = $exist['firstname']; 
+                        $view = new View("dashboard","back");
                     }
                     else
                     {
@@ -54,7 +33,7 @@ class User {
                     $view = new View("login","front"); // On crée une page de vue en appelant le partial Login avec un template front (front.tpl.php)    
                     $view->assign("user", $user);
                 }
->>>>>>> feature_seb
+
         }
         else{
             header('Location: dashboard');
